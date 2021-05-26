f62379754180b234c8d7d36b606ff114d85e0cb0
Kyungrae Kim <v-kkim@microsoft.com>
2021-05-25 23:50:34 -0700

    azure cli test 12

--- config.sh
+++ config.sh
@@ -2,4 +2,4 @@
 
 workspaceObjectId=$(az ad sp list --display-name 'synapse-test-actions' --query '[].objectId' -o tsv)
 
-az role assignment list --assignee f629d07b-8c93-49d6-9dc7-e5e79a39e3a4
+az role assignment list --assignee 'f629d07b-8c93-49d6-9dc7-e5e79a39e3a4'
