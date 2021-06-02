dc899722138398beb9d2a3967cc9b2689e8ff7b6
Kyungrae Kim <v-kkim@microsoft.com>
2021-06-02 00:02:36 -0700

    synapse trigger list command

--- config.sh
+++ config.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-workspaceObjectId=$(az ad sp list --display-name 'synapse-test-actions' --query '[].objectId' -o tsv)
+# workspaceObjectId=$(az ad sp list --display-name 'synapse-test-actions' --query '[].objectId' -o tsv)
 
-az role assignment list --assignee 'fdb798ab-987e-4e10-92d0-be511d22dcab'
+# az role assignment list --assignee 'fdb798ab-987e-4e10-92d0-be511d22dcab'
+
+echo "Running az synpase trigger list command..."
+az synapse trigger list --workspace-name synapse-test-actions
