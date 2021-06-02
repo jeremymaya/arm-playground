3d8edaeff2486ca10f245845daa9b1a9f084f321
Kyungrae Kim <v-kkim@microsoft.com>
2021-06-02 01:11:54 -0700

    bash flag execution

--- config.sh
+++ config.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# workspaceObjectId=$(az ad sp list --display-name 'synapse-test-actions' --query '[].objectId' -o tsv)
-
-# az role assignment list --assignee 'fdb798ab-987e-4e10-92d0-be511d22dcab'
-
 echo "Running az synpase trigger list command..."
 triggers=$(az synapse trigger list --workspace-name synapse-test-actions --query '[].name' -o tsv)
 
 for trigger in "${triggers[@]}"
 do
+    if [ stage == "build"]; then
     echo "Stopping trigger ${trigger}..."
     az synapse trigger stop --workspace-name synapse-test-actions --name $trigger
+    else
     echo "Starting trigger ${trigger}..."
     az synapse trigger start --workspace-name synapse-test-actions --name $trigger
+    fi
 done
